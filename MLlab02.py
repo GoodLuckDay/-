e575acbcac58cf1686f07c22630495aac9a3c247
GoodLuckDay <haon4658@gmail.com>
2017-12-06 02:20:31 +0900

    Tensorflow Saver Example implement

--- MLlab02.py
+++ MLlab02.py
@@ -1,12 +1,20 @@
 import tensorflow as tf
+import numpy as np
 
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
+def MinMaxScaler(data):
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+    return numerator / (denominator + 1e-7)
+
+
+data = np.array([[6,185],[8,218],[5,175],[10,336],[9,417],[2,61],[9,528],[10,818],[5,439],[2,40],[6,147],[7,448],[10,348],[7,393],[7,82],[9,443],[4,59]])
+x_train = data[:,0]
+y_train = data[:,[-1]]
 
 W = tf.Variable(tf.random_normal([1]),name = 'weight')
 b = tf.Variable(tf.random_normal([1]),name = 'bias')
 
-hypothesis = W * x_train + b
+hypothesis = x_train * W + b
 cost = tf.reduce_mean(tf.square(hypothesis - y_train))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
@@ -19,3 +27,4 @@ for step in range(2001):
     sess.run(train)
     if step % 20 == 0:
         print(step, sess.run(cost), sess.run(W), sess.run(b))
+print(sess.run(hypothesis))
---
+++ SaverExample.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+
+data = np.loadtxt("./data.csv", dtype="float32", delimiter=",", unpack=True)
+
+x_data = np.transpose(data[0:2])
+y_data = np.transpose(data[2:])
+
+X = tf.placeholder(tf.float32, [None, 2])
+Y = tf.placeholder(tf.float32, [None, 3])
+
+global_step = tf.Variable(0, trainable=False, name='global_step')
+
+W1 = tf.Variable(tf.random_uniform([2,10], -1, 1))
+L1 = tf.nn.relu(tf.matmul(X, W1))
+
+W2 = tf.Variable(tf.random_uniform([10, 20], -1, 1))
+L2 = tf.nn.relu(tf.matmul(L1, W2))
+
+W3 = tf.Variable(tf.random_uniform([20, 3], -1, 1))
+model = tf.matmul(L2, W3)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=model))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost)
+
+with tf.Session() as sess:
+    saver = tf.train.Saver(tf.global_variables())
+    ckpt = tf.train.get_checkpoint_state("./model")
+
+    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    for step in range(2):
+        sess.run(train_op, feed_dict={X:x_data, Y:y_data})
+
+        print("Step : %d, " % sess.run(global_step), "Cost %.3f" % sess.run(cost, feed_dict={X:x_data, Y:y_data}))
+
+    saver.save(sess, './model/dnn.ckpt', global_step = global_step)
+
+    prediction = tf.argmax(model, 1)
+    target = tf.argmax(Y, 1)
+    print('예측값:', sess.run(prediction, feed_dict={X: x_data}))
+    print('실제값:', sess.run(target, feed_dict={Y: y_data}))
+
+    is_correction = tf.equal(prediction, target)
+    accuracy = tf.reduce_mean(tf.cast(is_correction, tf.float32))
+    print('정확도: %.2f' % sess.run(accuracy * 100, feed_dict={X: x_data, Y: y_data}))
+
